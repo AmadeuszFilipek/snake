15fbeaa010166299daf8ec78647574d1d18d6994
Amadeusz <amadeusz.filipek@gmail.com>
2020-01-30 17:48:43 +0100

    Grid cells are now aligned

--- app.js
+++ app.js
@@ -16,24 +16,52 @@ function init() {
       },
 
       async mounted() {
-         let grid = initializeGrid(this.AppState.grid_size)
+         let grid = this.initializeGrid(this.AppState.grid_size)
          this.AppState.grid = grid
       },
 
       computed: {
-         
+         gridSectionPercents: function() {
+            let sections = []
+            let percent = 100 / this.AppState.grid_size;
+            for (let i=0; i < this.AppState.grid_size; i++) {
+               sections.push(percent + '%')
+            }
+            return sections
+         },
+         gridStructureStyle: function() {
+            return {
+               'display': 'grid', 
+               'grid-template-columns': this.gridSectionPercents.reduce(function(x,y){return x + ' ' + y})
+            }
+         },
+         gridColumnStyle: function() {
+            return {
+               'display': 'grid',
+               'grid-template-rows': this.gridSectionPercents.reduce(function(x,y){return x + ' ' + y})
+            }
+         }
       },
 
       methods: {
+         
+         gridCellStyle: function(row, col) {
+            return {
 
+            }
+         },
          initializeGrid(grid_size) {
-            grid = []
-            for (row = 0; row < grid_size; row++) {
+            let grid = []
+            for (let row = 0; row < grid_size; row++) {
                grid.push([])
-               for (col = 0; col < grid_size; col++) {
+               for (let col = 0; col < grid_size; col++) {
                   grid[row].push(0)
                }
             }
+            return grid
+         },
+         generateNewApple(snake) {
+            
          }
 
       }
