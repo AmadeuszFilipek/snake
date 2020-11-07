4c4f7d1d8a7a5749036740411e58a866f7bc83b9
Amadeusz <amadeusz.filipek@gmail.com>
2020-11-07 19:40:07 +0100

    just a manual testing script for evolution

--- validate_evolution.py
+++ validate_evolution.py
@@ -6,32 +6,37 @@ import math
 def rastrigin_function(arguments):
    result = 0
    for x in arguments:
-      result += x ** 2 - math.cos(18 * x)
+      result += x ** 2 - 10 * math.cos(18 * x)
    
+   result += 10 * len(arguments)
+
    return result
 
 if __name__ == "__main__":
 
    crossover_operators = [
-      xso.shuffle_crossover(mixing_rate=0.1),
-      xso.average_crossover(ratio=0.2)
+      # xso.average_crossover(ratio=0.2),
+      # xso.shuffle_crossover(mixing_rate=0.5),
+      xso.gamma_weighted_crossover()
    ]
 
    mutation_operators = [
-      mo.gauss_mutate(mu=0, sigma=0.05),
-      mo.spike_mutate(),
-      mo.negate_mutate(),
-      mo.univariate_mutate(mu=0, sigma=0.05),
+      # mo.univariate_mutate(mu=0, sigma=0.05),
+      mo.gauss_rate_mutate(mu=0, sigma=1),
+      # mo.gauss_mutate(mu=0, sigma=2),
       mo.null_mutate()
+      # mo.spike_mutate(),
+      # mo.negate_mutate(),
    ]
  
    results = evo.evolution_optimise(
+      bounds=evo.Bounds(-5, 5),
        target=rastrigin_function,
        crossover_operators=crossover_operators,
        mutation_operators=mutation_operators,
        dimensions=100,
        population_size=100,
-       generations=100,
+       generations=1000,
        workers=1
    )
 
